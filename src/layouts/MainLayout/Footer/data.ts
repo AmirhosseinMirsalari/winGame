b7d62aaf8f8a0ff9777fd2e6edb969d8c9632519
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-16 13:40:01 +0330

    create mock data for footer

---
+++ src/layouts/MainLayout/Footer/data.ts
@@ -0,0 +1,72 @@
+export const companyInfo = [
+  {
+    name: "درباره ما",
+    route: "/about-us",
+  },
+  {
+    name: "ارتباط با ما",
+    route: "/contact-us",
+  },
+  {
+    name: "بلاگ",
+    route: "/blog",
+  },
+  {
+    name: "همکاری با ما",
+    route: "/contact-us",
+  },
+];
+export const letUsHelpYou = [
+  {
+    name: "سفارشات",
+    route: "/user/orders",
+  },
+  {
+    name: "دانلود ها",
+    route: "#",
+  },
+  {
+    name: "آدرس ها",
+    route: "/user/addresses",
+  },
+  {
+    name: "اطلاعات حساب کاربری",
+    route: "/user/personal-info",
+  },
+];
+export const quickLinks = [
+  {
+    name: "پیشنهادات ویژه",
+    route: "/shop",
+  },
+  {
+    name: "گیفت کارت",
+    route: "#",
+  },
+  {
+    name: "تعهدات ما",
+    route: "#",
+  },
+  {
+    name: "قوانین و ضوابط",
+    route: "#",
+  },
+];
+export const userFullLinks = [
+  {
+    name: "جدید ترین محصولات",
+    route: "/shop?sort=latest",
+  },
+  {
+    name: "بالاترین امتیازات",
+    route: "/shop?sort=rating",
+  },
+  {
+    name: "محصولات ویژه",
+    route: "/shop",
+  },
+  {
+    name: "ارزان ترین محصولات",
+    route: "/shop?sort=latest",
+  },
+];
