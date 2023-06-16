8523a6b1208bd742daafe5e373653af7e134a72c
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-16 15:11:31 +0330

    create mock data for navbar section

---
+++ src/layouts/MainLayout/Navbar/data.ts
@@ -0,0 +1,99 @@
+export const navbarItems = [
+  {
+    id: 1,
+    name: "صفحه اصلی",
+    route: "/",
+  },
+  {
+    id: 2,
+    name: "فروشگاه",
+    route: "/shop",
+  },
+  {
+    id: 3,
+    name: "بلاگ",
+    route: "/blog",
+  },
+  {
+    id: 4,
+    name: "ارتباط با ما",
+    route: "/contact-us",
+  },
+  {
+    id: 5,
+    name: "درباره ما",
+    route: "/about-us",
+  },
+];
+
+export const shopMenuItems = [
+  {
+    id: 1,
+    name: "ساعت هوشمند",
+  },
+  {
+    id: 2,
+    name: "اسمارت فون و آیپد",
+  },
+  {
+    id: 3,
+    name: "بازی های ویدویی",
+  },
+  {
+    id: 4,
+    name: "تکنولوژی",
+  },
+  {
+    id: 5,
+    name: "اکسسوری موبایل",
+  },
+  {
+    id: 6,
+    name: "کامپیوتر و لبتاب",
+  },
+  {
+    id: 7,
+    name: "دوربین عکاسی",
+  },
+  {
+    id: 8,
+    name: "واقعیت مجازی",
+  },
+  {
+    id: 9,
+    name: "اسپیکر",
+  },
+];
+
+export const shopMenuCategories = [
+  {
+    id: 1,
+    name: "هدفون",
+    img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233710/digita-images/static/ps8mlfms424bmjin5x0x.png",
+  },
+  {
+    id: 2,
+    name: "اسمارت فون و آیپد",
+    img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233843/digita-images/static/b8t1ncn0hdll7xujjf8v.png",
+  },
+  {
+    id: 3,
+    name: "تکنولوژی ها",
+    img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233867/digita-images/static/jshwc6pd7h0rewchzkm3.png",
+  },
+  {
+    id: 4,
+    name: "ساعت هوشمند",
+    img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233890/digita-images/static/eltln72flhuwguetwfrq.png",
+  },
+  {
+    id: 5,
+    name: "دوربین عکاسی",
+    img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233910/digita-images/static/bz6ixnxiclhfmspbvn82.png",
+  },
+  {
+    id: 6,
+    name: "اسپیکر",
+    img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233939/digita-images/static/hnxjeziopulvyum56g9h.png",
+  },
+];
