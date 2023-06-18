46cffc1ab152ed22aba2cb7a45ec27aefeb3fd2e
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-18 22:43:03 +0330

    add auth and cart reducer to rootReducer

--- src/redux/_rootReducer.ts
+++ src/redux/_rootReducer.ts
@@ -1,5 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import cartReducer from "./cart/cartSlice";
+import authReducer from "./auth/authSlice";
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+    cart: cartReducer,
+    auth: authReducer,
+
+});
 
 export default reducers;
