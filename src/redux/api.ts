2bbfd99423d90c4e4ac2c6d4917c17bb691ea4a0
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-18 07:08:10 +0330

    create api endpoint url to fetch data in redux toolkit

---
+++ src/redux/api.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8000/api",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  }),
+
+  tagTypes: ["Product", "Article", "Wishlist", "Cart", "User", "Users", "Orders"],
+  endpoints: () => ({}),
+});
