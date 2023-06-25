b4e8946e164827a28fb5274c5c39caabdc652c17
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-25 19:07:06 +0330

    The design of the button on the banners

---
+++ src/components/Home/Products/Components/ButtonCard/ButtonCard.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { StyledBtn } from "../../styles";
+
+const ButtonCard = () => {
+  return (
+    <Box sx={{ margin: "2.2rem 0" }}>
+      <StyledBtn variant="contained">مشاهده محصولات</StyledBtn>
+    </Box>
+  );
+};
+
+export default ButtonCard;
