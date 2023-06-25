3e8885ec967f592597bff401b46672b902b166ba
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-25 10:33:57 +0330

    add compare reducer to root reducer

--- src/redux/_rootReducer.ts
+++ src/redux/_rootReducer.ts
@@ -1,11 +1,12 @@
-import { combineReducers } from "@reduxjs/toolkit";
 import cartReducer from "./cart/cartSlice";
+import compareReducer from "./compare/compareSlice";
 import authReducer from "./auth/authSlice";
+import { combineReducers } from "@reduxjs/toolkit";
 
 const reducers = combineReducers({
-    cart: cartReducer,
-    auth: authReducer,
-
+  cart: cartReducer,
+  compare: compareReducer,
+  auth: authReducer,
 });
 
 export default reducers;
