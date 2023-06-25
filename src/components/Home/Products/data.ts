b6338ca46d0949f22b39a7f94ef4b752447a9bcc
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-25 19:08:56 +0330

    create mock data for products card

---
+++ src/components/Home/Products/data.ts
@@ -0,0 +1,16 @@
+export const productEdition = [
+  {
+    id: 1,
+    title: "دوربین های عکاسی",
+    name: "برای ثبت لحظات خاص ",
+    image:
+      "https://cdn.mos.cms.futurecdn.net/YhGYD7PF3nvs2XGfZAD2G7-1200-80.jpg.webp",
+  },
+  {
+    id: 2,
+    title: "لوارم صوتی",
+    name: "کیفیت بالا برای ضبط صدا",
+    image:
+      "https://wp-assets.rss.com/blog/wp-content/uploads/2021/07/10111146/podcast-script-microphone-1024x683.jpg",
+  },
+];
