817ba67580d65c2500ae96ca8d35db89a8aed76a
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-06-25 10:31:51 +0330

    create products section in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-import Header from "../components/Home/Header/Header";
+import Header from "components/Home/Header/Header";
+import Products from "components/Home/Products/Products";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
         <title>ویـن گـیم</title>
       </Head>
       <Header />
+      <Products />
     </>
   );
 };
