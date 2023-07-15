7e3ed46882232ceb09e34267ac8a0a7005025327
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-15 13:18:39 +0330

    create shop cart section responsive

---
+++ src/layouts/MainLayout/Navbar/ShopCart/ShopCart.tsx
@@ -0,0 +1,92 @@
+import { Box, Button, Divider, Link, List, Typography } from "@mui/material";
+import  * as RouterLink  from "next/link";
+import { useAppSelector } from "redux/store";
+import { shopCartWrapper } from "../styles";
+import { useGetAllCartItemQuery } from "redux/cart/cartApi";
+import ShopCartItem from "./ShopCartItem/ShopCartItem";
+import { getSubtotal } from "utils/getSubtotal";
+
+function ShopCart() {
+  const { user } = useAppSelector((state) => state.reducer.auth);
+  const { cartList, subtotal } = useAppSelector((state) => state.reducer.cart);
+
+
+  const { data: cartData, isLoading, isError } = useGetAllCartItemQuery(undefined, { skip: !!!user });
+  const cart = cartData?.data?.products ?? [];
+
+  const cartItems = user ? cart : cartList;
+
+  let cartSubtotal = subtotal;
+  if (user) {
+    cartSubtotal = getSubtotal(cartItems);
+  }
+
+  return (
+    <Box sx={shopCartWrapper} className="shop-cart">
+      <List sx={{ maxHeight: "360px", overflow: "overlay" }}>
+        {cartItems.length === 0 && !isLoading && !isError && (
+          <Box width="300px" marginY={3}>
+            <Typography textAlign="center" textTransform="capitalize" color="black">
+              سبد خرید شما خالی است
+            </Typography>
+          </Box>
+        )}
+        {cartItems.length !== 0 &&
+          !isLoading &&
+          cartItems?.map((item) => (
+            <ShopCartItem
+              key={item?._id}
+              id={item?._id}
+              name={item?.name}
+              price={item?.price}
+              quantity={item?.quantity}
+              image={item?.productId?.image ?? ""}
+              productId={item?.productId?._id ?? ""}
+            />
+          ))}
+        <Divider />
+      </List>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "13px",
+        }}
+      >
+        <Typography variant="subtitle2" color={"secondary"} fontWeight={400}>
+          مجموع :
+        </Typography>
+        <Typography variant="subtitle2" color={"primary"}>
+          {cartSubtotal?.toFixed(2)}
+        </Typography>
+      </Box>
+      <Divider />
+      <Box
+        sx={{
+          display: "flex",
+          paddingY: "20px",
+          justifyContent: "space-between",
+          gap: "10px",
+        }}
+      >
+        <Link component={RouterLink} href="/cart" sx={{ textDecoration: "none", width: "50%" }}>
+          <Button
+            variant="contained"
+            fullWidth={true}
+            color="error"
+            sx={{
+              "&:hover": { backgroundColor: "#333333" },
+            }}
+          >
+            مشاهده سبد خرید
+          </Button>
+        </Link>
+        <Button variant="contained" sx={{ width: "50%" }} onClick={()=>{!user ? navigate.push("/?login=open") : navigate.push("/checkout")  }}>
+        {!user ? "ورود و " : ""} تسویه حساب
+        </Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default ShopCart;
