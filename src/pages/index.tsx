6982a9d57155984b17b1ce16a110cd44a23e4f07
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-15 23:17:10 +0330

    add special section to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Header from "components/Home/Header/Header";
 import Products from "components/Home/Products/Products";
 import ShopByCategories from "components/Home/ShopByCategories/ShopByCategories";
+import Special from "components/Home/Special/Special";
+
 
 const Home = () => {
   return (
@@ -12,6 +14,8 @@ const Home = () => {
       <Header />
       <Products />
       <ShopByCategories />
+      <Special />
+
     </>
   );
 };
