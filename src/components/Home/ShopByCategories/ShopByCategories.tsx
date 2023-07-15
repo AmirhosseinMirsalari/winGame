f0f370b1385c6f1570630e97695ec3cedc179cc0
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-15 19:41:23 +0330

    create shop by category section

---
+++ src/components/Home/ShopByCategories/ShopByCategories.tsx
@@ -0,0 +1,24 @@
+import { Box } from "@mui/material";
+import Slider from "./Slider/Slider";
+import { boxStyles, Title, WrapperBox } from "./styles";
+import { useInView } from "react-intersection-observer";
+
+function ShopByCategories() {
+  const { ref, inView } = useInView({ triggerOnce: true });
+
+  return (
+    <WrapperBox className={inView ? "slideInFromBottom" : ""} ref={ref}>
+      <Box sx={boxStyles}>
+        <Title variant="h2">خرید بر اساس دسته بندی</Title>
+        <img
+          className="loading"
+          src="https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665232874/digita-images/static/uej2dnhrgldg1jaztexn.png"
+          alt="loading"
+        />
+        <Slider />
+      </Box>
+    </WrapperBox>
+  );
+}
+
+export default ShopByCategories;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Header from "components/Home/Header/Header";
 import Products from "components/Home/Products/Products";
+import ShopByCategories from "components/Home/ShopByCategories/ShopByCategories";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       </Head>
       <Header />
       <Products />
+      <ShopByCategories />
     </>
   );
 };
