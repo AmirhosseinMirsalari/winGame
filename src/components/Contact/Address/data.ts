9557c128b98d2921c7c184ee4ee5bebe63af16d4
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-16 23:15:04 +0330

    create address section in contact us page and use mock data

---
+++ src/components/Contact/Address/data.ts
@@ -0,0 +1,22 @@
+export const ourAddress = [
+  {
+    id: 1,
+    title: "آدرس",
+    subtitle1: "Remote and Full Time",
+  },
+  {
+    id: 2,
+    title: "ایمیل",
+    subtitle1: "amir.tk7@gmail.com",
+  },
+  {
+    id: 3,
+    title: "شماره تماس",
+    subtitle1: "(+98) 933-280-0956",
+  },
+  {
+    id: 4,
+    title: "ساعت پاسخگویی",
+    subtitle1: "24/7",
+  },
+];
