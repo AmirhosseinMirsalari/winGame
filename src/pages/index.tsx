0902593485b5266688ce2db8a9949b5048a00c43
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-16 09:23:43 +0330

    create our blog section in home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Header from "components/Home/Header/Header";
 import Products from "components/Home/Products/Products";
 import ShopByCategories from "components/Home/ShopByCategories/ShopByCategories";
 import Special from "components/Home/Special/Special";
+import OurBlog from "components/Home/OurBlog/OurBlog";
 
 
 const Home = () => {
@@ -15,7 +16,7 @@ const Home = () => {
       <Products />
       <ShopByCategories />
       <Special />
-
+      <OurBlog />
     </>
   );
 };
