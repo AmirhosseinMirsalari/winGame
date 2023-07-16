dd1f24ac26f0cf7d7b4b1a50ee6e500d0545ece0
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-16 09:24:54 +0330

    create blog card component and style it

---
+++ src/components/Home/OurBlog/BlogCard/Styles/index.tsx
@@ -0,0 +1,20 @@
+import { styled } from "@mui/material/styles";
+import { Box } from "@mui/material";
+
+export const Card = styled(Box)(({ theme }) => ({
+  width: "100%",
+  position: "relative",
+  img: {
+    maxWidth: "100%",
+    borderRadius: "20px",
+    height: "auto",
+    aspectRatio: "1.4",
+    objectFit: "cover",
+  },
+}));
+export const textStyle = { fontSize: "15px", color: "#777777" };
+export const iconStyle = {
+  fontSize: "1rem",
+  marginRight: "5px",
+  color: "#777777",
+};
