3a61a3da92f37f7a186e125c48b1a3389bb1dc48
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-19 06:22:00 +0330

    fix bug when user is not logged in

--- src/pages/checkout/_middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export default async function middleware(req, res) {
-
-    if (!req.cookies.access_token) {
-        return NextResponse.redirect("http://localhost:3000/?login=open");
-    }
-    return NextResponse.next();
-}
--- src/pages/checkout/index.tsx
+++ src/pages/checkout/index.tsx
@@ -1,5 +1,16 @@
 import { useRef, ChangeEvent, useState } from "react";
-import { Box, Button, Checkbox, Collapse, Container, Divider, FormGroup, Grid, Modal, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Checkbox,
+  Collapse,
+  Container,
+  Divider,
+  FormGroup,
+  Grid,
+  Modal,
+  Typography,
+} from "@mui/material";
 import {
   ShippingDatePicker,
   OrderTable,
@@ -34,15 +45,17 @@ import { useUpdateUserMutation } from "redux/user/userApi";
 import Head from "next/head";
 
 function Checkout() {
-  const { user,role } = useAppSelector((state) => state.reducer.auth);
-  
+  const { user, role } = useAppSelector((state) => state.reducer.auth);
+
   const [displayCurrentAddress, setDisplayCurrentAddress] = useState(false);
   const [displayAddresses, setDisplayAddresses] = useState(false);
   const [displayCoupon, setDisplayCoupon] = useState(false);
   const [selectedDate, setSelectedDate] = useState("");
   const [couponValue, setCouponValue] = useState("");
   const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("cash");
-  const [currentAddress, setCurrentAddress] = useState<IAddress | null>(user?.addresses[0] ?? null);
+  const [currentAddress, setCurrentAddress] = useState<IAddress | null>(
+    user?.addresses[0] ?? null
+  );
   const [addressError, setAddressError] = useState("");
   const [date, setDate] = useState<{
     endValue: Date | null;
@@ -146,147 +159,193 @@ function Checkout() {
     navigate.push("/order-confirm");
   }
   if (!user) {
-    navigate.push("/?login=open")
+    navigate.push("/?login=open");
   }
   if (!cart && isLoading) {
     return <Loading full />;
   }
-  return (
-    <>
-    <Head>
-        <title>تسویه حساب</title>
-    </Head>
-      <Breadcrumbs title="تسویه حساب" />
-      <Box bgcolor={"white"}>
-        <Container maxWidth={"lg"} sx={{ py: "50px" }}>
-          {addressError && !currentAddress && (
-            <Box
-              sx={{
-                backgroundColor: "common.digitaRed",
-                padding: "10px 15px",
-                marginBottom: "40px",
-              }}
-              id="error"
-            >
-              <Typography sx={{ color: "white ", fontWeight: 500, display: "flex", alignItems: "center", gap: "3px" }}>
-                <Close />
-                {addressError}
-              </Typography>
-            </Box>
-          )}
-          <Grid container spacing={5}>
-            <Grid item xs={12} md={6}>
-              <Grid container rowSpacing={2} sx={{ padding: "25px", border: "2px solid #eaeaea" }}>
-                <Grid item xs={12}>
-                  <Box sx={checkoutTitleWrapper}>
-                    <Typography component={"h2"}>اطلاعات پرداخت</Typography>
-                  </Box>
-                </Grid>
+  if (user) {
+    return (
+      <>
+        <Head>
+          <title>تسویه حساب</title>
+        </Head>
+        <Breadcrumbs title="تسویه حساب" />
+        <Box bgcolor={"white"}>
+          <Container maxWidth={"lg"} sx={{ py: "50px" }}>
+            {addressError && !currentAddress && (
+              <Box
+                sx={{
+                  backgroundColor: "common.digitaRed",
+                  padding: "10px 15px",
+                  marginBottom: "40px",
+                }}
+                id="error"
+              >
+                <Typography
+                  sx={{
+                    color: "white ",
+                    fontWeight: 500,
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "3px",
+                  }}
+                >
+                  <Close />
+                  {addressError}
+                </Typography>
+              </Box>
+            )}
+            <Grid container spacing={5}>
+              <Grid item xs={12} md={6}>
+                <Grid
+                  container
+                  rowSpacing={2}
+                  sx={{ padding: "25px", border: "2px solid #eaeaea" }}
+                >
+                  <Grid item xs={12}>
+                    <Box sx={checkoutTitleWrapper}>
+                      <Typography component={"h2"}>اطلاعات پرداخت</Typography>
+                    </Box>
+                  </Grid>
 
-                <Grid item xs={12}>
-                  <CurrentAddress currentAddress={currentAddress} setDisplayCurrentAddress={setDisplayCurrentAddress} />
-                </Grid>
-                <Grid item xs={12}>
-                  <Collapse in={displayCurrentAddress}>
-                    <>
-                      <AddressForm
-                        addAddress={addAddress}
-                        buttonText={currentAddress ? "ویرایش" : "اضافه"}
-                        currentAddress={currentAddress}
-                      />
-                    </>
-                  </Collapse>
-                </Grid>
-                <Grid item xs={12}>
-                  <Box sx={addressTitleStyles} onClick={displayAddressesToggler}>
-                    <Typography component="h4">ارسال به آدرس متفاوت</Typography>
-                    <Add className="icon" />
-                  </Box>
-                  <Divider sx={{ width: "97%", mt: "4px", mb: "20px" }} />
-                  <Modal open={displayAddresses} onClose={() => setDisplayAddresses(false)}>
+                  <Grid item xs={12}>
+                    <CurrentAddress
+                      currentAddress={currentAddress}
+                      setDisplayCurrentAddress={setDisplayCurrentAddress}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Collapse in={displayCurrentAddress}>
+                      <>
+                        <AddressForm
+                          addAddress={addAddress}
+                          buttonText={currentAddress ? "ویرایش" : "اضافه"}
+                          currentAddress={currentAddress}
+                        />
+                      </>
+                    </Collapse>
+                  </Grid>
+                  <Grid item xs={12}>
                     <Box
+                      sx={addressTitleStyles}
+                      onClick={displayAddressesToggler}
+                    >
+                      <Typography component="h4">
+                        ارسال به آدرس متفاوت
+                      </Typography>
+                      <Add className="icon" />
+                    </Box>
+                    <Divider sx={{ width: "97%", mt: "4px", mb: "20px" }} />
+                    <Modal
+                      open={displayAddresses}
+                      onClose={() => setDisplayAddresses(false)}
+                    >
+                      <Box
+                        sx={{
+                          ...cartModal,
+                          ...addressModalWrapper,
+                        }}
+                      >
+                        <Box>
+                          <Box
+                            className="close-button"
+                            onClick={() => setDisplayAddresses(false)}
+                          >
+                            <Close />
+                          </Box>
+                          {user?.addresses.map((item) => (
+                            <Address
+                              key={item?._id}
+                              address={item}
+                              selectAddress={selectAddress}
+                            />
+                          ))}
+                        </Box>
+                      </Box>
+                    </Modal>
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Typography>چه روزی می خواهید تحویل بگیرید؟</Typography>
+                    <ShippingDatePicker date={date} setDate={setDate} />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormGroup>
+                      <StyledFormControlLabel
+                        control={<Checkbox onChange={couponToggler} />}
+                        label="کد تخفیف دارید؟"
+                      />
+                    </FormGroup>
+                    <Divider sx={{ width: "97%", mt: "4px", mb: "20px" }} />
+                    <Collapse in={displayCoupon}>
+                      <>
+                        <Coupon setCouponValue={setCouponValue} />
+                      </>
+                    </Collapse>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <OrderNotes notesRef={notesRef} />
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Grid
+                  container
+                  rowSpacing={2}
+                  sx={{ padding: "25px", border: "2px solid #eaeaea" }}
+                >
+                  <Grid item xs={12}>
+                    <Box sx={checkoutTitleWrapper}>
+                      <Typography component={"h2"}>سفارش شما</Typography>
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <OrderTable
+                      cartItems={cart?.products!}
+                      subtotal={subtotal}
+                      currentAddress={currentAddress}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <PaymentMethod
+                      setSelectedPaymentMethod={setSelectedPaymentMethod}
+                      selectedPaymentMethod={selectedPaymentMethod}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography
+                      component={"p"}
                       sx={{
-                        ...cartModal,
-                        ...addressModalWrapper,
+                        color: "common.digitaGrey3",
+                        fontSize: "14px",
+                        textAlign: "justify",
                       }}
                     >
-                      <Box>
-                        <Box className="close-button" onClick={() => setDisplayAddresses(false)}>
-                          <Close />
-                        </Box>
-                        {user?.addresses.map((item) => (
-                          <Address key={item?._id} address={item} selectAddress={selectAddress} />
-                        ))}
-                      </Box>
-                    </Box>
-                  </Modal>
-                </Grid>
-
-                <Grid item xs={12}>
-                    <Typography>چه روزی می خواهید تحویل بگیرید؟</Typography>
-                  <ShippingDatePicker date={date} setDate={setDate} />
-                </Grid>
-                <Grid item xs={12}>
-                  <FormGroup>
-                    <StyledFormControlLabel control={<Checkbox onChange={couponToggler} />} label="کد تخفیف دارید؟" />
-                  </FormGroup>
-                  <Divider sx={{ width: "97%", mt: "4px", mb: "20px" }} />
-                  <Collapse in={displayCoupon}>
-                    <>
-                      <Coupon setCouponValue={setCouponValue} />
-                    </>
-                  </Collapse>
-                </Grid>
-                <Grid item xs={12}>
-                  <OrderNotes notesRef={notesRef} />
+                      اطلاعات شخصی و پرداخت شما طبق قوانین حریم شخصی وین گیم،
+                      محفوظ می ماند
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      variant="contained"
+                      sx={checkoutProceedBtn}
+                      onClick={submitOrderHandler}
+                      disabled={selectedPaymentMethod === "paypal"}
+                    >
+                      {selectedPaymentMethod === "cash"
+                        ? "سفارش"
+                        : "پرداخت آنلاین"}
+                    </Button>
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <Grid container rowSpacing={2} sx={{ padding: "25px", border: "2px solid #eaeaea" }}>
-                <Grid item xs={12}>
-                  <Box sx={checkoutTitleWrapper}>
-                    <Typography component={"h2"}>سفارش شما</Typography>
-                  </Box>
-                </Grid>
-                <Grid item xs={12}>
-                  <OrderTable cartItems={cart?.products!} subtotal={subtotal} currentAddress={currentAddress} />
-                </Grid>
-                <Grid item xs={12}>
-                  <PaymentMethod
-                    setSelectedPaymentMethod={setSelectedPaymentMethod}
-                    selectedPaymentMethod={selectedPaymentMethod}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Typography
-                    component={"p"}
-                    sx={{
-                      color: "common.digitaGrey3",
-                      fontSize: "14px",
-                      textAlign: "justify",
-                    }}
-                  >
-                    اطلاعات شخصی و پرداخت شما طبق قوانین حریم شخصی وین گیم، محفوظ می ماند
-                  </Typography>
-                </Grid>
-                <Grid item xs={12}>
-                  <Button
-                    variant="contained"
-                    sx={checkoutProceedBtn}
-                    onClick={submitOrderHandler}
-                    disabled={selectedPaymentMethod === "paypal"}
-                  >
-                    {selectedPaymentMethod === "cash" ? "سفارش" : "پرداخت آنلاین"}
-                  </Button>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Container>
-      </Box>
-    </>
-  );
+          </Container>
+        </Box>
+      </>
+    );
+  }
 }
 
 export default Checkout;
