72bb7eecfbd3ad44ca342922b7b1e194172dce30
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-27 21:29:37 +0330

    create Categories Filter to sort product with their categories

---
+++ src/components/Shop/CategoriesFilter/CategoriesFilter.tsx
@@ -0,0 +1,64 @@
+import { Box, List, ListItem, Typography } from "@mui/material";
+import { productCategories } from "../data";
+import { FilterListItemText, FilterTitleWrapper } from "../styles";
+
+interface Props {
+  addQueryParams: (filter: string, name: string) => () => void;
+  categoryQueryParams: string | null;
+}
+
+function CategoriesFilter({ addQueryParams, categoryQueryParams }: Props) {
+  const selectedCategories = categoryQueryParams?.split("/");
+
+  
+  return (
+    <Box
+      sx={{
+        padding: "20px",
+        border: "1px solid #e9e9e9",
+        borderRadius: "30px",
+        boxShadow:"0px 0px 10px rgba(0, 0, 0, 0.1)"
+      }}
+    >
+      <FilterTitleWrapper className="underline">
+        <Typography
+          component={"h4"}
+          variant="body1"
+          color="primary"
+          fontWeight={600}
+        >
+          دسته بندی محصولات
+        </Typography>
+      </FilterTitleWrapper>
+      <List>
+        {productCategories.map(({ id, name }) => {
+          let selected = categoryQueryParams
+            ? selectedCategories?.indexOf(name) === -1
+            : true;
+
+          return (
+            <ListItem
+              key={id}
+              sx={{
+                position: "relative",
+                paddingY: "4px",
+              }}
+              onClick={addQueryParams("category", name)}
+            >
+              <FilterListItemText
+                sx={{
+                  color: selected ? "" : "#f03637",
+                  direction: "ltr",
+                }}
+              >
+                {name}
+              </FilterListItemText>
+            </ListItem>
+          );
+        })}
+      </List>
+    </Box>
+  );
+}
+
+export default CategoriesFilter;
