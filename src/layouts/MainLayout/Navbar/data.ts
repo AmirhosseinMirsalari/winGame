b09372e562c4689a2842118287f842732b183f2c
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-29 14:22:29 +0330

    edit data in shop menu

--- src/layouts/MainLayout/Navbar/data.ts
+++ src/layouts/MainLayout/Navbar/data.ts
@@ -29,71 +29,67 @@ export const navbarItems = [
 export const shopMenuItems = [
   {
     id: 1,
-    name: "smart watch",
+    name: "ساعت هوشمند",
   },
   {
     id: 2,
-    name: "smartphone & ipad",
+    name: "موبایل هوشمند",
   },
   {
     id: 3,
-    name: "audio & video game",
-  },
-  {
-    id: 4,
-    name: "technologies",
+    name: "بازی های ویدویی",
   },
   {
     id: 5,
-    name: "phone accessories",
+    name: "اکسسوری موبایل",
   },
   {
     id: 6,
-    name: "PC & laptop",
+    name: "کامپیوتر و لپتاب",
   },
   {
     id: 7,
-    name: "camera & phone",
+    name: "دوربین عکاسی",
   },
   {
     id: 8,
-    name: "virtual reality glasses ",
+    name: "واقعیت مجازی",
   },
   {
     id: 9,
-    name: "bluetooth speaker ",
+    name: "اسپیکر بلوتوثی",
   },
 ];
 
 export const shopMenuCategories = [
   {
     id: 1,
-    name: "headphone",
+    name: "هدفون",
     img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233710/digita-images/static/ps8mlfms424bmjin5x0x.png",
   },
   {
     id: 2,
-    name: "smartphone & ipad",
+    name: "گوشی هوشمند",
     img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233843/digita-images/static/b8t1ncn0hdll7xujjf8v.png",
   },
   {
     id: 3,
-    name: "technologies",
+    name: "تکنولوژی",
     img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233867/digita-images/static/jshwc6pd7h0rewchzkm3.png",
   },
   {
     id: 4,
-    name: "smart watch",
+    name: "ساعت هوشمند",
     img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233890/digita-images/static/eltln72flhuwguetwfrq.png",
   },
   {
     id: 5,
-    name: "camera photo",
+    name: "دوربین عکاسی",
     img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233910/digita-images/static/bz6ixnxiclhfmspbvn82.png",
   },
   {
     id: 6,
-    name: "bluetooth speaker",
+    name: "اسپیکر بلوتوثی",
     img: "https://res.cloudinary.com/dmgb7kvmn/image/upload/v1665233939/digita-images/static/hnxjeziopulvyum56g9h.png",
   },
 ];
