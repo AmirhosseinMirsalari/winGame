0caf15dd750eb176553b215adac216692dc3d2f6
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-31 00:38:16 +0330

    Creating a component that can be used when the required value is empty

---
+++ src/components/EmptyList/EmptyList.tsx
@@ -0,0 +1,20 @@
+import { Box, Typography } from "@mui/material";
+import { EmptyWishlistWrapper } from "./styles";
+
+interface T {
+  title: string;
+  image: string;
+}
+
+const EmptyList = ({ title, image }: T) => {
+  return (
+    <EmptyWishlistWrapper>
+      <Box component="img" src={image} alt="empty" className="itemImg" />
+      <Typography sx={{ fontWeight: "400", fontSize: "17px" }}>
+         مقدار {title}  خالی است
+      </Typography>
+    </EmptyWishlistWrapper>
+  );
+};
+
+export default EmptyList;
