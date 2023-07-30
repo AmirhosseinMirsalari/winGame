8b520e284362b241f451392a09ed8e37c1b382dc
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-30 18:34:49 +0330

    create and responsive sidebar in layout user pages

---
+++ src/layouts/UserLayout/Sidebar/Sidebar.tsx
@@ -0,0 +1,57 @@
+import React, { Fragment } from "react";
+import {
+  Divider,
+  Icon,
+  List,
+  ListItemIcon,
+  ListItemText,
+  Box,
+} from "@mui/material";
+import { userSidebarOptions } from "../data";
+import SidebarTop from "./SidebarTop/SidebarTop";
+// import MyOrders from "components/UserStatus/MyOrders/MyOrders";
+import Link from "next/link";
+import { UserItem } from "../../../../styles/user";
+import { useRouter } from "next/router";
+
+const Sidebar = () => {
+  const router = useRouter();
+
+  return (
+    <>
+      <SidebarTop />
+      <Divider sx={{ borderColor: "common.panelBorderGrey" }} />
+
+      {/* <MyOrders sidebar={true} /> */}
+      <Divider
+        sx={{ borderColor: "common.panelBorderGrey", display: { md: "none" } }}
+      />
+      <List>
+        {userSidebarOptions.map(({ id, title, route, icon }) => (
+          <Fragment key={id}>
+            {title === "settings" && <Divider sx={{ marginY: "10px" }} />}
+            <UserItem className={title === "وضعیت" ? "hidden" : ""}>
+              <Box
+                onClick={() => router.push(`${route}`)}
+                className={`${route === router.asPath && "active"} link`}
+              >
+                <ListItemIcon>{React.createElement(icon)}</ListItemIcon>
+                <ListItemText
+                  sx={{ textAlign: "start" }}
+                  primaryTypographyProps={{
+                    fontSize: "16px",
+                    fontWeight: 600,
+                    textTransform: "capitalize",
+                  }}
+                  primary={title}
+                />
+              </Box>
+            </UserItem>
+          </Fragment>
+        ))}
+      </List>
+    </>
+  );
+};
+
+export default Sidebar;
