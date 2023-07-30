4ba55b2bf3c08438004d4a55c5e595392fd37de4
Amirhossein Mirsalari <amir.tk7@gmail.com>
2023-07-30 18:39:27 +0330

    make personal info page for user to show user information and user can change password

---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { Box, Grid, Typography } from "@mui/material";
+import Sidebar from "./Sidebar/Sidebar";
+import { ArrowBack } from "@mui/icons-material";
+import { useAppSelector } from "redux/store";
+import Link from "next/link";
+import { CardWrapper,wrapper } from "../../../styles/user";
+import General from "components/Settings/General/General";
+import Password from "components/Settings/Password/Password";
+
+const PersonalInfo = () => {
+  const { user } = useAppSelector((state) => state.reducer.auth);
+
+
+  const [activePage, setActivePage] = useState("general");
+
+  const activePageHandler = (page: string) => () => {
+    setActivePage(page);
+  };
+  
+
+  return (
+    <Box sx={wrapper}>
+      <CardWrapper>
+        <Box sx={{ display: "flex", gap: "10px" }}>
+          <Link href="/user">
+            <Box
+              sx={{
+                display: { md: "none" },
+                textDecoration: "none",
+                color: "common.digitaBlack",
+              }}
+            >
+              <ArrowBack />
+            </Box>
+          </Link>
+
+          <Typography sx={{ fontWeight: "bold", fontSize: "18px" }}>
+            اطلاعات شخصی
+          </Typography>
+        </Box>
+        <Grid container>
+          <Grid item xs={12} lg={3} mb={4}>
+            <Sidebar
+              activePageHandler={activePageHandler}
+              activePage={activePage}
+            />
+          </Grid>
+          <Grid item xs={12} lg={9}>
+            {activePage === "general" && <General user={user!} />}
+            {activePage === "password" && (
+              <Password id={user?._id!} role={user?.role!} />
+            )}
+          </Grid>
+        </Grid>
+      </CardWrapper>
+    </Box>
+  );
+};
+
+export default PersonalInfo;
---
+++ src/pages/user/personal-info.tsx
@@ -0,0 +1,12 @@
+import UserInfo from "components/UserInfo/UserInfo";
+import UserLayout from "layouts/UserLayout/UserLayout";
+
+function InfoPage() {
+  return (
+    <UserLayout>
+      <UserInfo />
+    </UserLayout>
+  );
+}
+
+export default InfoPage;
